ee5f7f2e30c0eaa48a0253a8aba4b102e17103a1
Princemth20 <58967741+Princemth20@users.noreply.github.com>
2020-06-22 21:58:38 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,294 +1,145 @@
-<?php
-include("header.php");
-include("mysql.php");
-$result = mysql_query("SELECT * FROM profile ORDER BY RAND()
-");
-if(isset($_SESSION["logid"]))
-{
-	header("Location: profile.php");
-}
-$i=0;
-while($row = mysql_fetch_array($result))
-  {
-$img[$i] = $row["image"];
-$uid[$i] = $row["userid"];
-$i++;
-  }
-  
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+ <head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <title>phpFreeChat - Demos</title>
+  <link rel="stylesheet" title="classic" type="text/css" href="../style/generic.css" />
+  <link rel="stylesheet" title="classic" type="text/css" href="../style/header.css" />
+  <link rel="stylesheet" title="classic" type="text/css" href="../style/footer.css" />
+  <link rel="stylesheet" title="classic" type="text/css" href="../style/menu.css" />
+  <link rel="stylesheet" title="classic" type="text/css" href="../style/content.css" />   
+ </head>
+ <body>
 
-?>
-<script language="javascript">
-
- function reg()
-{
-		ml = document.registration.email.value;
-		pos1 = ml.indexOf("@")
-		pos = ml.indexOf(" ")
-		pos2 = (pos1+1)
-		server = ml.substring(pos2);
-		server_pos = server.lastIndexOf(".")
-		reqtype = server.substring(server_pos+1)
-		type_end = reqtype.substring(reqtype.length-1)
-		
-	if(document.registration.fname.value=="")
-	{
-		alert("Please enter first name");
-		document.registration.fname.focus();
-		document.registration.fname.select();
-		return false;
-	}
-	else if(document.registration.lname.value=="")
-	{
-		alert("Please enter last name");
-		document.registration.lname.focus();
-		document.registration.lname.select();
-		return false;
-	}
-	else if(ml == "")
-	{
-		document.registration.email.focus();
-		document.registration.email.select();
-		alert("Email cannot be blank");
-		return false;                
-	}
-
-
-		else if(ml.indexOf("@")==-1)
-		{
-			document.registration.email.focus();
-			document.registration.email.select();
-			alert("The Email Address must contain '@' sign");
-			return false;  
-		}
-		else if(pos1<1)
-		{
-			document.registration.email.focus();
-			document.registration.email.select();
-			alert("Email address cannot start with '@' sign");
-			return false;  
-		}  
-		else if(ml.indexOf(".")==-1)
-		{
-			document.registration.email.focus();
-			document.registration.email.select();
-			alert("The Email Address must contain '.' sign");
-			return false;  
-		}
-		else if(pos!=-1)
-		{
-			document.registration.email.focus();
-			document.registration.email.select();
-			alert("The Email Address cannot contain spaces");
-			return false;  
-		}
-		else if(server.indexOf("@")!=-1)
-		{
-			document.registration.email.focus();
-			document.registration.email.select();
-			alert("A valid Email must contain only one '@' sign");
-			return false;  
-		} 
-		else if(server.indexOf(".")==0)
-		{
-			document.registration.email.focus();
-			document.registration.email.select();
-			alert("There should some text between '@' and '.' sign");
-			return false;  
-		} 
-		else if(reqtype=="")
-		{  
-			document.registration.email.focus();
-			document.registration.email.select();
-			alert("Email Id should end with character(like .com,.net,.org)");
-			return false;  
-		}
-		else if(type_end.toUpperCase()<"A" || type_end.toUpperCase()>"Z")
-		{
-			document.registration.email.focus();
-			document.registration.email.select();
-			alert("Email Id should not end with number or symbol");
-			return false;  
-		}
-		
-	else if(document.registration.pass.value=="")
-	{
-		alert("Please enter password");
-		document.registration.pass.focus();
-		document.registration.pass.select();
-		return false;
-	}
-	else if(document.registration.pass.value.length<8)
-	{
-		alert("The minimum length of the password is 8 characters...");
-		document.registration.pass.focus();
-		document.registration.pass.select();
-		return false;
-	}
-	else if(document.registration.pass.value != document.registration.cpass.value)
-    {
-		alert("Password and confirm password is not matching");
-		document.registration.cpass.value="";
-		document.registration.cpass.focus();
-		document.registration.pass.select();
-    	return false;
-	}
-	else if(document.registration.Date.value=="DD")
-		{
-			alert("Please select Date");
-		document.registration.Date.focus();
-		return false;
-		}
-		else if(document.registration.month.value=="Month")
-		{
-			alert("Please select Month");
-		document.registration.month.focus();
-		return false;
-		}
-		else if(document.registration.Year.value=="Year")
-		{
-			alert("Please select Year");
-		document.registration.Year.focus();
-		return false;
-		}
-		
-	else
-		{
-		return true;
-		}	
-}
-	</script>
-	
-<?php
-include("mysql.php");
-$result1 = mysql_query("SELECT * FROM college");
-
-if(isset($_POST["button2"]))
-{
-$datee="$_POST[Year]-$_POST[month]-$_POST[Date]";
-$sql="INSERT INTO stuacc (firstname,lastname,email,password,confirmpassword,iam,dob)
-VALUES
-('$_POST[fname]','$_POST[lname]','$_POST[email]','$_POST[pass]','$_POST[cpass]','$_POST[gen]','$datee')";
-
-if (!mysql_query($sql,$con))
-  {
-  die('Error: ' . mysql_error());
-  }
-  else
-  {
-$_SESSION["res"] =  "User Registered Successfully...";
-header("Location: login.php");
-  }
-}
-?>
-
-<center>
-<div class=container>
-
-<!-- head -->
-<?php include("head.php"); ?>
-
-<!-- navigation menu -->
-<?php include("menu.php"); ?>
-
-<div style="padding: 10px; text-align: left;">
-<!-- body  content -->
-<table cellpadding=10 width=100%>
-<tr>
-	<td width="70%">
-		<img src="images/pic.png" width="518" height="137" border="0" alt="">
-		<h2>Meet New Friends </h2>
-
-		<table width=100%>
-        
-		<tr valign=top align=center>
-			<td><a href=""><img src="<?php echo $img[0] ; ?>" width="100" height="150" border="0" alt=""></a><br></td>
-			<td><a href=""><img src="<?php echo $img[1] ; ?>" width="100" height="150" border="0" alt=""></a><br></td>
-			<td><a href=""><img src="<?php echo $img[2] ; ?>" width="100" height="150" border="0" alt=""></a><br></td>
-			<td><a href=""><img src="<?php echo $img[3] ; ?>" width="100" height="150" border="0" alt=""></a><br></td>
-       
-		</tr>
-		</table>
-
-		<h2>About Us</h2>		
-		The Campus networking website is an online community designed to make your Campus life more active and stimulating. The Campus network can help you maintain existing relationships with college students and share pictures and messages, and establish new ones by reaching out to people you've never met before, and also using this site user can share Knowledge's, education related books, question/answer, etc. </td>
-	<td width="30%" valign="top"><img src="images/imaa.png" width="100%" height="199"><br>
-	<h2>Register Free</h2>
-		<table width="208" border="0" cellpadding="5" cellspacing="0">
-<form id="registration" name="registration" method="post" action="index.php" onSubmit="return reg()">
-			<tr>
-			  <td width="59">First Name</td>
-			  <td width="91"><input name="fname" type="text" id="fname" value="" size="20"></td>
-			  </tr>
-			<tr>
-			  <td>Last Name</td>
-			  <td><input name="lname" type="text" id="lname" value="" size="20"></td>
-			  </tr>
-			<tr>
-			  <td>Email</td>
-			  <td><input name="email" type="text" id="email" value="" size="20"></td>
-			  </tr>
-			<tr>
-			  <td>Password</td>
-			  <td><input name="pass" type="Password" id="pass" size="20"></td>
-			  </tr>
-			<tr>
-			  <td>Confirm</td>
-			  <td><input name="cpass" type="Password" id="cpass" size="20"></td>
-			  </tr>
-			<tr>
-			  <td>I am</td>
-			  <td><label>
-			    <input name="gen" type="radio"  value="Male" checked="checked" />
-			    Male</label>
-                <input type="radio" name="gen"  value="Female" />
-                Female</td>
-			  </tr>
-			<tr>
-			  <td colspan="2">DOB&nbsp;&nbsp;&nbsp; 
-			    <select name="Date" >
-			      <option>DD</option>
-			      <?php
-for($i=1; $i<= 31; $i++)
-{
-echo "<option value='$i'>$i</option>";
-}
-?>
-			      </select>
-			    <select name="month">
-			      <option>Month</option>
-			      <option value="01">Jan</option>
-			      <option value="02">Feb</option>
-			      <option value="03">Mar</option>
-			      <option value="04">Apr</option>
-			      <option value="05">May</option>
-			      <option value="06">Jun</option>
-			      <option value="07">Jul</option>
-			      <option value="08">Aug</option>
-			      <option value="09">Sep</option>
-			      <option value="10">Oct</option>
-			      <option value="11">Nov</option>
-			      <option value="12">Dec</option>
-			      </select>
-			    <select name="Year">
-			      <option value="Year">Year</option>
-			      <?php
-for($i=1975; $i< 2011; $i++)
-{
-echo "<option value='$i'>$i</option>";
-}
-?>
-		        </select></td>
-			  </tr>
-			<tr> 
-				<td><input class=button type="submit" name="button2" value="Register"></td>
-			</tr>
-
-			</form>
-		  </table>
-	</td>
-</tr>
-</table>
-
+<div class="header">
+      <img alt="" src="../style/logo.gif" class="logo2" />
 </div>
 
-<?php include("footer.php"); ?>
+<div class="menu">
+      <ul>
+        <li class="sub title">General</li>
+        <li>
+          <ul class="sub">
+            <li class="item">
+              <a href="../index.php">Index</a>
+            </li>
+          </ul>
+        </li>
+        <li class="sub title">Demos</li>
+        <li>
+          <ul class="sub">
+            <li class="item">
+              <a href="#demo-misc">Miscellaneous</a>
+            </li>
+            <li class="item">
+              <a href="#demo-themes">Themes</a>
+            </li>
+            <li class="item">
+              <a href="#demo-translations">Translations</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <p class="partner">
+        <a href="http://www.dhavalcollege.com"><img alt="phpfreechat" src="../style/logo_88x31.gif" /></a><br/>
+      </p>
+</div>
+
+<div class="content">
+
+<h2 id="demo-misc">Miscellaneous</h2>
+
+<ul>
+      <li><a href="demo1_simple.php">demo1 - with default parameters</a></li>
+      <li><a href="demo2_simple_with_params.php">demo2 - with few customized parameters</a></li>
+      <li><a href="demo3_client.php">demo3 - with separated client/server files</a></li>
+      <li><a href="demo4_simulate_slow_server.php">demo4 - simulates slow server responses</a></li>
+      <li><a href="demo5_customized_style.php">demo5 - with a customized style (theme)</a></li>
+      <li><a href="demo8_with_a_iso-8859-1_encoded_page.php">demo8 - a chat with a ISO-8859-1 encoded page</a></li>
+      <li><a href="demo9_with_a_utf8_encoded_nickname.php">demo9 - a chat with a UTF-8 encoded nickname (none latin)</a></li>
+      <li><a href="demo15_multiple_channel.php">demo15 - a chat with multiple/dynamic channels</a></li>
+      <li><a href="demo21_with_hardcoded_urls.php">demo21 - with hardcoded url: data_public_url, client_script_url ... (interesting for strange server configuration)</a></li>
+      <li><a href="demo27_customized_command.php">demo27 - a customized command (/roll)</a></li>
+      <li><a href="demo34_add_a_link_on_nicknames.php">demo34 - shows how to add a link on the nicknames list</a></li>
+
+      <li><a href="demo31_show_who_is_online-chat.php">demo31 - shows how to get the connected users list (chat script)</a></li>
+      <li><a href="demo31_show_who_is_online-whoisonline.php">demo31 - shows how to get the connected users list (whoisonline script)</a></li>
+      <li><a href="demo32_show_last_messages-chat.php">demo32 - shows how to get the last posted messages (chat script)</a></li>
+      <li><a href="demo32_show_last_messages-showlastmsg.php">demo32 - shows how to get the last posted messages (showlastmsg script)</a></li>
+<!--      <li><a href="demo35_shared_memory.php">demo35 - shows how to use the shared memory container</a> (not yet working)</li>-->
+      <li><a href="demo43_change_the_nicknames_colors.php">demo43 - shows how to change the nicknames automatic colors</a></li>
+      <li><a href="demo50_customized_usermetadata.php">demo50 - shows how to use user metadata : add avatar (images) to each connected users</a></li>
+      <li><a href="demo55_mysql_container.php">demo55 - shows how to use the mysql container</a></li>
+      <li><a href="demo65_metadata_and_html.php">demo65 - shows how to start a chat in a child popup window and how to display in this page a complete user profil comming from a chat request (user click) </a></li>
+</ul>
+
+
+<h2 id="demo-themes">Themes</h2>
+
+<ul>
+      <li><a href="demo12_phoenity_smiley_theme.php">demo12 - customized smiley theme: phoenity</a></li>
+      <li><a href="demo17_cerutti_smiley_theme.php">demo17 - customized smiley theme: cerutti</a></li>
+      <li><a href="demo18_phpbb2_smiley_theme.php">demo18 - customized smiley theme: phpbb2</a></li>
+      <li><a href="demo28_blune_theme.php">demo28 - customized theme: blune</a></li>
+      <li><a href="demo28_mini_blune_theme.php">demo28 - customized theme: blune mini</a></li>
+      <li><a href="demo44_green_theme.php">demo44 - customized theme: green</a></li>
+      <li><a href="demo49_msn_smiley_theme.php">demo49 - customized smiley theme: msn</a></li>
+      <li><a href="demo51_zilveer_theme.php">demo51 - customized theme: zilveer</a></li>
+</ul>
+
+<h2 id="demo-translations">Translations</h2>
+    <ul>
+      <li><a href="demo6_in_french.php">demo6 - french translation of the chat</a></li>
+      <li><a href="demo7_in_english.php">demo7 - english translation of the chat</a></li>
+      <li><a href="demo10_in_chinese.php">demo10 - chinese translation of the chat</a></li>
+      <li><a href="demo13_in_german_informal_language.php">demo13 - german (informal) translation of the chat</a></li>
+      <li><a href="demo14_in_german_formal_language.php">demo14 - german (formal) translation of the chat</a></li>
+      <li><a href="demo16_in_arabic.php">demo16 - arabic translation of the chat</a></li>
+      <li><a href="demo19_in_japanese.php">demo19 - japanese translation of the chat</a></li>
+      <li><a href="demo20_in_brazilian_portuguese.php">demo20 - portuguese from brazil translation of the chat</a></li>
+      <li><a href="demo22_in_serbian_croatian.php">demo22 - serbian croatian translation of the chat</a></li>
+      <li><a href="demo23_in_italian.php">demo23 - italian translation of the chat</a></li>
+      <li><a href="demo24_in_spanish.php">demo24 - spanish translation of the chat</a></li>
+      <li><a href="demo25_in_indonesian.php">demo25 - indonesian translation of the chat</a></li>
+      <li><a href="demo26_in_swedish.php">demo26 - swedish translation of the chat</a></li>
+      <li><a href="demo29_in_russian.php">demo29 - russian translation of the chat</a></li>
+      <li><a href="demo33_in_portuguese_from_portugal.php">demo33 - portuguese from portugal translation of the chat</a></li>
+      <li><a href="demo36_in_ukrainian.php">demo36 - ukrainian translation of the chat</a></li>
+      <li><a href="demo37_in_dutch_from_netherlands.php">demo37 - dutch from netherlands translation of the chat</a></li>
+      <li><a href="demo38_in_norwegian_bokmal.php">demo38 - norwegian bokmal translation of the chat</a></li>
+      <li><a href="demo39_in_bosnian.php">demo39 - bosnian translation of the chat</a></li>
+      <li><a href="demo40_in_turkish.php">demo40 - turkish translation of the chat</a></li>
+      <li><a href="demo41_in_greek.php">demo41 - greek translation of the chat</a></li>
+      <li><a href="demo42_in_chinese_from_taiwan.php">demo42 - Chinese from taiwan (traditional Chinese) translation of the chat</a></li>
+      <li><a href="demo45_in_bulgarian.php">demo45 - Bulgarian translation of the chat</a></li>
+      <li><a href="demo46_in_hungarian.php">demo46 - Hungarian translation of the chat</a></li>
+      <li><a href="demo47_in_polish.php">demo47 - Polish translation of the chat</a></li>
+      <li><a href="demo52_in_bangla.php">demo52 - Bangla translation of the chat</a></li>
+      <li><a href="demo53_in_armenian.php">demo53 - Armenian translation of the chat</a></li>
+      <li><a href="demo54_in_esperanto.php">demo54 - Esperanto translation of the chat</a></li>
+      <li><a href="demo56_in_romanian.php">demo56 - Romanian translation of the chat</a></li>
+      <li><a href="demo57_in_korean.php">demo57 - Korean translation of the chat</a></li>
+      <li><a href="demo58_in_danish.php">demo58 - Danish translation of the chat</a></li>
+      <li><a href="demo59_in_norwegian_nynorsk.php">demo59 - Norwegian Nynorsk translation of the chat</a></li>
+      <li><a href="demo60_in_vietnamese.php">demo60 - Vietnamese translation of the chat</a></li>
+      <li><a href="demo61_in_croatian.php">demo61 - Croatian translation of the chat</a></li>
+      <li><a href="demo62_in_belgian_dutch.php">demo62 - Belgian Dutch translation of the chat</a></li>
+      <li><a href="demo63_in_thai.php">demo63 - Thai translation of the chat</a></li>
+      <li><a href="demo64_in_galician.php">demo64 - Galician translation of the chat</a></li>
+      <li><a href="demo66_in_occitan.php">demo66 - Occitan translation of the chat</a></li>
+    </ul>
+
+</div>
+     
+<div class="footer">
+  <span class="partners">phpFreeChat partners:</span>
+  <a href="http://www.jeu-gratuit.net">jeux gratuits</a> |
+  <a href="http://jeux-flash.jeu-gratuit.net">jeux flash</a> |
+  <a href="http://www.pronofun.com">pronofun</a> |
+  <a href="http://areno.jeu-gratuit.net">areno</a> |
+  <a href="http://www.micropolia.com">micropolia</a> |
+  <a href="http://www.zeitoun.net">zeitoun</a> |
+  <a href="http://federation.jeu-gratuit.net">federation</a>
+</div>
+
+</body></html>
